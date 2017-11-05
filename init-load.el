2c0fce2d62af01c4454f04241baa2dc84a5518c0
xujing <jink_xu@foxmail.com>
2017-11-05 15:55:03 +0800

    rm elc file, and update elisp config

--- init-load.el
+++ init-load.el
@@ -1,7 +1,7 @@
 (add-to-list 'load-path "~/.emacs.d/my-config/")
 (add-to-list 'load-path "~/.emacs.d/other-packages/")
 
-;; (require 'my-emacs-package-config)
+(require 'my-emacs-package-config)
 ;;;; package config
 ;; (load "~/.emacs.d/my-emacs-package-config.el")
 
@@ -30,6 +30,8 @@
 ;;; ;;; C-lang
 (require 'my-c-mode-config)
 
+;;; ;;; lisp-lang
+(require 'my-lisp-config)
 
 ;; gdb
 (require 'my-gdb-config)
--- my-config/my-emacs-package-config.el
+++ my-config/my-emacs-package-config.el
@@ -1,5 +1,5 @@
-
 (require 'package)
+
 (setq package-load-list '(all))
 (package-initialize)
 ;; (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
@@ -9,24 +9,44 @@
                          ("melpa" . "https://melpa.org/packages/")))
 
 
+;; (if (null (string-match-p "Mac" emacs-build-system))
+;; 	(defun package-update-load-path ()
+;; 	  "Update the load path for newly installed packages."
+;; 	  (interactive)
+;; 	  (let ((package-dir (expand-file-name package-user-dir)))
+;; 		(mapc (lambda (pkg)
+;; 				(let ((stem (symbol-name (car pkg)))
+;; 					  (version "")
+;; 					  (first t)
+;; 					  path)
+;; 				  (mapc (lambda (num)
+;; 						  (if first
+;; 							  (setq first nil)
+;; 							(setq version (format "%s." version)))
+;; 						  (setq version (format "%s%s" version num)))
+;; 						(aref (cdr pkg) 0))
+;; 				  (setq path (format "%s/%s-%s" package-dir stem version))
+;; 				  (add-to-list 'load-path path)))
+;; 			  package-alist)))
+
+;;   (defun package-update-load-path ()
+;; 	"Update the load path for newly installed packages."
+;; 	(interactive)
+;; 	(mapc (lambda (pkg)
+;; 			(let ((path (package-desc-dir (car (cdr pkg)))))
+;; 			  (add-to-list 'load-path path)))
+;; 		  package-alist))
+;;   )
+
+
 (defun package-update-load-path ()
   "Update the load path for newly installed packages."
   (interactive)
-  (let ((package-dir (expand-file-name package-user-dir)))
-    (mapc (lambda (pkg)
-            (let ((stem (symbol-name (car pkg)))
-                  (version "")
-                  (first t)
-                  path)
-              (mapc (lambda (num)
-                      (if first
-                          (setq first nil)
-                        (setq version (format "%s." version)))
-                      (setq version (format "%s%s" version num)))
-                    (aref (cdr pkg) 0))
-              (setq path (format "%s/%s-%s" package-dir stem version))
-              (add-to-list 'load-path path)))
-          package-alist)))
+  (mapc (lambda (pkg)
+		  (let ((path (package-desc-dir (car (cdr pkg)))))
+			(add-to-list 'load-path path)))
+		package-alist))
+
 
 (package-update-load-path)
 
